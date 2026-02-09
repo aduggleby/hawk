c7170ce31cd6c08ccf2bc360695582aff9ce1d61
Alexander Duggleby <151508+aduggleby@users.noreply.github.com>
2026-02-09 08:02:04 +0100

    Add Tailwind v4 styles and modern minimal UI

---
+++ Hawk.Web/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './Pages/**/*.{cshtml,cs}',
+    './Areas/**/*.{cshtml,cs}',
+    './Services/**/*.cs',
+    './Data/**/*.cs',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
